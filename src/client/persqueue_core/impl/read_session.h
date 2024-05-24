5260bce67e0872c9383c385cd00b1e684e8b8dbe
Bulat Gayazov <bylatgr@gmail.com>
2024-05-24 17:19:16 +0000

    Moved "fix misspell" commit from ydb repo

--- src/client/persqueue_core/impl/read_session.h
+++ src/client/persqueue_core/impl/read_session.h
@@ -789,16 +789,16 @@ public:
 
     bool Close(const TASessionClosedEvent<UseMigrationProtocol>& event, TDeferredActions<UseMigrationProtocol>& deferred) {
         TWaiter waiter;
-        std::vector<TRawPartitionStreamEventQueue<UseMigrationProtocol>> defferedDelete;
+        std::vector<TRawPartitionStreamEventQueue<UseMigrationProtocol>> deferredDelete;
         with_lock(TParent::Mutex) {
             if (TParent::Closed) {
                 return false;
             }
-            defferedDelete.reserve(TParent::Events.size());
+            deferredDelete.reserve(TParent::Events.size());
             while (!TParent::Events.empty()) {
                 auto& event = TParent::Events.front();
                 if (!event.IsEmpty()) {
-                    defferedDelete.push_back(event.PartitionStream->ExtractQueue());
+                    deferredDelete.push_back(event.PartitionStream->ExtractQueue());
                 }
                 TParent::Events.pop();
             }
@@ -808,7 +808,7 @@ public:
         }
 
         // Delayed deletion is necessary to avoid deadlock with PushEvent
-        defferedDelete.clear();
+        deferredDelete.clear();
 
         TReadSessionEventInfo<UseMigrationProtocol> info(event);
         ApplyHandler(info, deferred);
