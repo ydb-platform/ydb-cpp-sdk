36ba73b47cc6f82e72b04d5b9810704eaae36371
Pavel Tsayukov <p.tsayukov@gmail.com>
2024-06-05 15:19:53 +0300

    Minor improvements (#180)

--- src/library/digest/lower_case/hash_ops.h
+++ src/library/digest/lower_case/hash_ops.h
@@ -2,7 +2,7 @@
 
 #include <string_view>
 
-// can be used for caseless hashes like: THashMap<std::string_view, T, TCIOps, TCIOps>
+// can be used for caseless hashes like: std::unordered_map<std::string_view, T, TCIOps, TCIOps>
 
 struct TCIOps {
     size_t operator()(const char* s) const noexcept;
