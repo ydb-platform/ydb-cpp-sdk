bf02bd921434785424f62c95293799ef263f5edf
ildar-khisambeev <ikhis@ydb.tech>
2024-06-05 19:11:19 +0000

    Moved "fix implicit cast offset to bool" commit from ydb repo

--- src/client/persqueue_public/include/read_events.h
+++ src/client/persqueue_public/include/read_events.h
@@ -352,7 +352,7 @@ struct TReadSessionEvent {
 
         std::string DebugString() const;
 
-        TDestroyPartitionStreamEvent(TPartitionStream::TPtr partitionStream, bool committedOffset);
+        TDestroyPartitionStreamEvent(TPartitionStream::TPtr partitionStream, ui64 committedOffset);
 
     private:
         TPartitionStream::TPtr PartitionStream;
