6aba311e2b4f55d613abc720618c1db75286fb19
st-shchetinin <st-shchetinin@ydb.tech>
2024-07-16 17:50:08 +0300

    THolder -> std::unique_ptr in config.h

--- src/library/logger/init_context/config.h
+++ src/library/logger/init_context/config.h
@@ -7,7 +7,7 @@ class TLogBackendCreatorInitContextConfig : public ILogBackendCreator::IInitCont
 public:
     TLogBackendCreatorInitContextConfig(const NConfig::TConfig& config);
     virtual bool GetValue(std::string_view name, std::string& var) const override;
-    virtual std::vector<THolder<IInitContext>> GetChildren(std::string_view name) const override;
+    virtual std::vector<std::unique_ptr<IInitContext>> GetChildren(std::string_view name) const override;
 
 private:
     const NConfig::TConfig& Config;
