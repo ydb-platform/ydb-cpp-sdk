ef92e4889cee4451fa6cd303f166d52b97a17791
st-shchetinin <st-shchetinin@ydb.tech>
2024-07-17 20:02:29 +0300

    THolder -> std::unique_ptr in iterator.h

--- src/util/folder/iterator.h
+++ src/util/folder/iterator.h
@@ -9,12 +9,16 @@
 #include <ydb-cpp-sdk/util/generic/ylimits.h>
 
 #include <string>
+#include <memory>
 
 /// Note this magic API traverses directory hierarchy
 
 class TDirIterator: public TInputRangeAdaptor<TDirIterator> {
     struct TFtsDestroy {
         static inline void Destroy(FTS* f) noexcept {
+            yfts_close(f);
+        }
+        void operator()(FTS* f) {
             yfts_close(f);
         }
     };
@@ -73,7 +77,7 @@ public:
         Trees_[1] = nullptr;
 
         ClearLastSystemError();
-        FileTree_.Reset(yfts_open(Trees_, Options_.FtsOptions, Options_.Cmp));
+        FileTree_.reset(yfts_open(Trees_, Options_.FtsOptions, Options_.Cmp));
 
         const int err = LastSystemError();
 
@@ -83,11 +87,11 @@ public:
     }
 
     inline FTSENT* Next() {
-        FTSENT* ret = yfts_read(FileTree_.Get());
+        FTSENT* ret = yfts_read(FileTree_.get());
 
         if (ret) {
             if ((size_t)(ret->fts_level + 1) > Options_.MaxLevel) {
-                yfts_set(FileTree_.Get(), ret, FTS_SKIP);
+                yfts_set(FileTree_.get(), ret, FTS_SKIP);
             }
         } else {
             const int err = LastSystemError();
@@ -101,12 +105,12 @@ public:
     }
 
     inline void Skip(FTSENT* ent) {
-        yfts_set(FileTree_.Get(), ent, FTS_SKIP);
+        yfts_set(FileTree_.get(), ent, FTS_SKIP);
     }
 
 private:
     TOptions Options_;
     std::string Path_;
     char* Trees_[2];
-    THolder<FTS, TFtsDestroy> FileTree_;
+    std::unique_ptr<FTS, TFtsDestroy> FileTree_;
 };
