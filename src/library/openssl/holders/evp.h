55ecc8dda420de57370cffe38631ce78f7fae195
st-shchetinin <st-shchetinin@ydb.tech>
2024-07-17 18:39:38 +0300

    THolder -> std::unique_ptr in evp.cpp

--- src/library/openssl/holders/evp.h
+++ src/library/openssl/holders/evp.h
@@ -5,9 +5,9 @@
 #include <openssl/evp.h>
 
 namespace NOpenSSL {
-    class TEvpCipherCtx : public THolder<EVP_CIPHER_CTX, EVP_CIPHER_CTX_new, EVP_CIPHER_CTX_free> {
+    class TEvpCipherCtx : public std::unique_ptr<EVP_CIPHER_CTX, EVP_CIPHER_CTX_new, EVP_CIPHER_CTX_free> {
     };
 
-    class TEvpMdCtx : public THolder<EVP_MD_CTX, EVP_MD_CTX_new, EVP_MD_CTX_free> {
+    class TEvpMdCtx : public std::unique_ptr<EVP_MD_CTX, EVP_MD_CTX_new, EVP_MD_CTX_free> {
     };
 }
