6a79e91d05ef1eee90d3b1717f95420c3c4e9062
st-shchetinin <st-shchetinin@ydb.tech>
2024-07-18 10:44:23 +0300

    change spaces in macroses

--- src/library/digest/argonish/internal/proxies/macro/proxy_macros.h
+++ src/library/digest/argonish/internal/proxies/macro/proxy_macros.h
@@ -29,13 +29,13 @@
         virtual size_t GetMemorySize() const override;                                                                \
                                                                                                                       \
     protected:                                                                                                        \
-        std::unique_ptr<IArgon2Base> argon2;                                                                                  \
+        std::unique_ptr<IArgon2Base> argon2;                                                                          \
     };
 
-#define ARGON2_INSTANCE_DECL(IS_val, mcost_val, threads_val)                                     \
-    if (mcost == mcost_val && threads == threads_val) {                                          \
-        argon2 = std::make_unique<TArgon2##IS_val<mcost_val, threads_val>>(atype, tcost, key, keylen); \
-        return;                                                                                  \
+#define ARGON2_INSTANCE_DECL(IS_val, mcost_val, threads_val)                                                \
+    if (mcost == mcost_val && threads == threads_val) {                                                     \
+        argon2 = std::make_unique<TArgon2##IS_val<mcost_val, threads_val>>(atype, tcost, key, keylen);      \
+        return;                                                                                             \
     }
 
 #define ARGON2_PROXY_CLASS_IMPL(IS)                                                                                           \
@@ -49,8 +49,8 @@
         ARGON2_INSTANCE_DECL(IS, 16, 1)                                                                                       \
         ARGON2_INSTANCE_DECL(IS, 32, 1)                                                                                       \
         ARGON2_INSTANCE_DECL(IS, 64, 1)                                                                                       \
-        ARGON2_INSTANCE_DECL(IS, 128, 1)                                                                                       \
-        ARGON2_INSTANCE_DECL(IS, 256, 1)                                                                                       \
+        ARGON2_INSTANCE_DECL(IS, 128, 1)                                                                                      \
+        ARGON2_INSTANCE_DECL(IS, 256, 1)                                                                                      \
         ARGON2_INSTANCE_DECL(IS, 512, 1)                                                                                      \
         ARGON2_INSTANCE_DECL(IS, 1024, 1)                                                                                     \
         ARGON2_INSTANCE_DECL(IS, 2048, 1)                                                                                     \
@@ -159,7 +159,7 @@
         virtual void Final(void* out, size_t outlen) override;                          \
                                                                                         \
     protected:                                                                          \
-        std::unique_ptr<IBlake2Base> blake2;                                                    \
+        std::unique_ptr<IBlake2Base> blake2;                                             \
     };
 
 #define BLAKE2B_PROXY_CLASS_IMPL(IS)                                                      \
@@ -168,14 +168,14 @@
             ythrow yexception() << "outlen equals 0 or too long";                         \
                                                                                           \
         if (key == nullptr) {                                                             \
-            blake2 = std::make_unique<TBlake2B<EInstructionSet::IS>>(outlen);                   \
+            blake2 = std::make_unique<TBlake2B<EInstructionSet::IS>>(outlen);             \
             return;                                                                       \
         }                                                                                 \
                                                                                           \
         if (!key || !keylen || keylen > BLAKE2B_KEYBYTES)                                 \
             ythrow yexception() << "key is null or too long";                             \
                                                                                           \
-        blake2 = std::make_unique<TBlake2B<EInstructionSet::IS>>(outlen, key, keylen);          \
+        blake2 = std::make_unique<TBlake2B<EInstructionSet::IS>>(outlen, key, keylen);    \
     }                                                                                     \
                                                                                           \
     void TBlake2BProxy##IS::Update(ui32 in) {                                             \
