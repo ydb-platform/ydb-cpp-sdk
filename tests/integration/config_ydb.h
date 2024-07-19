6949854562f8d3cbd2f7f1238d559771a5c5f7d5
st-shchetinin <st-shchetinin@ydb.tech>
2024-07-19 22:24:49 +0300

    basic_example integration test

---
+++ tests/integration/config_ydb.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_YDB_H_IN
+#define CONFIG_YDB_H_IN
+
+#include <string>
+
+const std::string endpoint = "localhost:2136";
+const std::string database = "/Root/test";
+ 
+#endif // CONFIG_YDB_H_IN
