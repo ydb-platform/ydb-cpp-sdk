816be4428b42dc862e53e498e55fdb6539531261
Bulat Gayazov <bylatgr@gmail.com>
2024-07-24 16:52:46 +0000

    Refactor codecs header

--- include/ydb-cpp-sdk/client/topic/codecs.h
+++ include/ydb-cpp-sdk/client/topic/codecs.h
@@ -1,13 +1,12 @@
 #pragma once
 
-#include <src/library/streams/zstd/zstd.h>
-#include <src/util/generic/buffer.h>
-#include <src/util/stream/buffer.h>
 #include <ydb-cpp-sdk/util/stream/mem.h>
 #include <ydb-cpp-sdk/util/stream/str.h>
-#include <src/util/stream/zlib.h>
 #include <ydb-cpp-sdk/util/stream/output.h>
 #include <ydb-cpp-sdk/util/system/spinlock.h>
+#include <ydb-cpp-sdk/util/generic/yexception.h>
+
+#include <src/util/generic/buffer.h>
 
 #include <unordered_map>
 #include <memory>
@@ -41,61 +40,21 @@ public:
 };
 
 class TGzipCodec final : public ICodec {
-    class TZLibToStringCompressor: private TEmbedPolicy<TBufferOutput>, public TZLibCompress {
-    public:
-        TZLibToStringCompressor(TBuffer& dst, ZLib::StreamType type, size_t quality)
-            : TEmbedPolicy<TBufferOutput>(dst)
-            , TZLibCompress(TEmbedPolicy::Ptr(), type, quality)
-        {
-        }
-    };
+    std::string Decompress(const std::string& data) const override;
 
-    std::string Decompress(const std::string& data) const override {
-        TMemoryInput input(data.data(), data.size());
-        std::string result;
-        TStringOutput resultOutput(result);
-        TZLibDecompress inputStreamStorage(&input);
-        TransferData(&inputStreamStorage, &resultOutput);
-        return result;
-    }
-
-    std::unique_ptr<IOutputStream> CreateCoder(TBuffer& result, int quality) const override {
-        return std::make_unique<TZLibToStringCompressor>(result, ZLib::GZip, quality >= 0 ? quality : 6);
-    }
+    std::unique_ptr<IOutputStream> CreateCoder(TBuffer& result, int quality) const override;
 };
 
 class TZstdCodec final : public ICodec {
-    class TZstdToStringCompressor: private TEmbedPolicy<TBufferOutput>, public TZstdCompress {
-    public:
-        TZstdToStringCompressor(TBuffer& dst, int quality)
-            : TEmbedPolicy<TBufferOutput>(dst)
-            , TZstdCompress(TEmbedPolicy::Ptr(), quality)
-        {
-        }
-    };
+    std::string Decompress(const std::string& data) const override;
 
-    std::string Decompress(const std::string& data) const override {
-        TMemoryInput input(data.data(), data.size());
-        std::string result;
-        TStringOutput resultOutput(result);
-        TZstdDecompress inputStreamStorage(&input);
-        TransferData(&inputStreamStorage, &resultOutput);
-        return result;
-    }
-
-    std::unique_ptr<IOutputStream> CreateCoder(TBuffer& result, int quality) const override {
-        return std::make_unique<TZstdToStringCompressor>(result, quality);
-    }
+    std::unique_ptr<IOutputStream> CreateCoder(TBuffer& result, int quality) const override;
 };
 
 class TUnsupportedCodec final : public ICodec {
-    std::string Decompress(const std::string&) const override {
-        throw yexception() << "use of unsupported codec";
-    }
+    std::string Decompress(const std::string&) const override;
 
-    std::unique_ptr<IOutputStream> CreateCoder(TBuffer&, int) const override {
-        throw yexception() << "use of unsupported codec";
-    }
+    std::unique_ptr<IOutputStream> CreateCoder(TBuffer&, int) const override;
 };
 
 class TCodecMap {
