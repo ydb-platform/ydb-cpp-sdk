99108f750fd379bb19be3994fd25f715576e9028
st-shchetinin <st-shchetinin@ydb.tech>
2024-07-29 10:14:34 +0300

    pathsplit -> std::filesystem::path & ui64 -> uint64_t

--- tests/integration/basic_example_it/basic_example.cpp
+++ tests/integration/basic_example_it/basic_example.cpp
@@ -1,8 +1,8 @@
 #include "basic_example.h"
 
-#include <src/util/folder/pathsplit.h>
 #include <ydb-cpp-sdk/json_value/ydb_json_value.h>
 
+#include <filesystem>
 #include <format>
 
 using namespace NYdb;
@@ -19,10 +19,10 @@ static std::string JoinPath(const std::string& basePath, const std::string& path
         return path;
     }
 
-    TPathSplitUnix prefixPathSplit(basePath);
-    prefixPathSplit.AppendComponent(path);
+    std::filesystem::path prefixPathSplit(basePath);
+    prefixPathSplit /= path;
 
-    return prefixPathSplit.Reconstruct();
+    return prefixPathSplit;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -176,7 +176,7 @@ static TStatus UpsertSimpleTransaction(TSession session, const std::string& path
 
 //! Shows usage of parameters in data queries.
 static TStatus SelectWithParamsTransaction(TSession session, const std::string& path,
-    ui64 seriesId, ui64 seasonId, std::optional<TResultSet>& resultSet)
+    uint64_t seriesId, uint64_t seasonId, std::optional<TResultSet>& resultSet)
 {
     auto query = std::format(R"(
         --!syntax_v1
@@ -216,7 +216,7 @@ static TStatus SelectWithParamsTransaction(TSession session, const std::string& 
 
 //! Shows usage of prepared queries.
 static TStatus PreparedSelectTransaction(TSession session, const std::string& path,
-    ui64 seriesId, ui64 seasonId, ui64 episodeId, std::optional<TResultSet>& resultSet)
+    uint64_t seriesId, uint64_t seasonId, uint64_t episodeId, std::optional<TResultSet>& resultSet)
 {
     // Once prepared, query data is stored in the session and identified by QueryId.
     // Local query cache is used to keep track of queries, prepared in current session.
@@ -268,7 +268,7 @@ static TStatus PreparedSelectTransaction(TSession session, const std::string& pa
 }
 
 //! Shows usage of transactions consisting of multiple data queries with client logic between them.
-static TStatus MultiStepTransaction(TSession session, const std::string& path, ui64 seriesId, ui64 seasonId,
+static TStatus MultiStepTransaction(TSession session, const std::string& path, uint64_t seriesId, uint64_t seasonId,
     std::optional<TResultSet>& resultSet)
 {
     auto query1 = std::format(R"(
