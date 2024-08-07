eebe4b02cb629982b4db2c857ccba1f7f64dfa1f
Pavel Tsayukov <p.tsayukov@gmail.com>
2024-08-08 01:17:45 +0300

    Fix string_transparent_hash_ut (#158)

--- util/generic/string_transparent_hash_ut.cpp
+++ util/generic/string_transparent_hash_ut.cpp
@@ -3,7 +3,12 @@
 #include "strbuf.h"
 
 #include <library/cpp/testing/unittest/registar.h>
-#include <library/cpp/containers/absl_flat_hash/flat_hash_set.h>
+
+#if __cplusplus < 202002L
+    #include <library/cpp/containers/absl_flat_hash/flat_hash_set.h>
+#else
+    #include <unordered_set>
+#endif
 
 #include <util/str_stl.h>
 
@@ -11,7 +16,15 @@ Y_UNIT_TEST_SUITE(StringHashFunctorTests) {
     Y_UNIT_TEST(TestTransparencyWithUnorderedSet) {
         // Using Abseil hash set because `std::unordered_set` is transparent only from C++20 (while
         // we stuck with C++17 right now).
-        absl::flat_hash_set<TString, THash<TString>, TEqualTo<TString>> s = {"foo"};
+        using TTransparentHashSet =
+    #if __cplusplus < 202002L
+            absl::flat_hash_set
+    #else
+            std::unordered_set
+    #endif
+            <TString, THash<TString>, TEqualTo<TString>>;
+
+        TTransparentHashSet s = {"foo"};
         // If either `THash` or `TEqualTo` is not transparent compilation will fail.
         UNIT_ASSERT_UNEQUAL(s.find(TStringBuf("foo")), s.end());
         UNIT_ASSERT_EQUAL(s.find(TStringBuf("bar")), s.end());
