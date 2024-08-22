a71c29e6a5c868fd63ef8db438c734ceaa4a6d1e
Pavel Tsayukov <p.tsayukov@gmail.com>
2024-08-22 11:26:01 +0300

    Revert "Fix string_transparent_hash_ut (#158)"
    This reverts commit eebe4b02cb629982b4db2c857ccba1f7f64dfa1f.

--- util/generic/string_transparent_hash_ut.cpp
+++ util/generic/string_transparent_hash_ut.cpp
@@ -3,12 +3,7 @@
 #include "strbuf.h"
 
 #include <library/cpp/testing/unittest/registar.h>
-
-#if __cplusplus < 202002L
-    #include <library/cpp/containers/absl_flat_hash/flat_hash_set.h>
-#else
-    #include <unordered_set>
-#endif
+#include <library/cpp/containers/absl_flat_hash/flat_hash_set.h>
 
 #include <util/str_stl.h>
 
@@ -16,15 +11,7 @@ Y_UNIT_TEST_SUITE(StringHashFunctorTests) {
     Y_UNIT_TEST(TestTransparencyWithUnorderedSet) {
         // Using Abseil hash set because `std::unordered_set` is transparent only from C++20 (while
         // we stuck with C++17 right now).
-        using TTransparentHashSet =
-    #if __cplusplus < 202002L
-            absl::flat_hash_set
-    #else
-            std::unordered_set
-    #endif
-            <TString, THash<TString>, TEqualTo<TString>>;
-
-        TTransparentHashSet s = {"foo"};
+        absl::flat_hash_set<TString, THash<TString>, TEqualTo<TString>> s = {"foo"};
         // If either `THash` or `TEqualTo` is not transparent compilation will fail.
         UNIT_ASSERT_UNEQUAL(s.find(TStringBuf("foo")), s.end());
         UNIT_ASSERT_EQUAL(s.find(TStringBuf("bar")), s.end());
