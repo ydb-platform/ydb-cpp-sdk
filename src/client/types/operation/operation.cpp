0a27948e5809e423460d35685880d9ccae83515f
Ilia Shakhov <pixcc@ydb.tech>
2024-08-23 20:11:15 +0000

    Moved commit "Add created_by to Operations API" from ydb repo

--- src/client/types/operation/operation.cpp
+++ src/client/types/operation/operation.cpp
@@ -21,7 +21,7 @@ public:
         : Id_(operation.id(), true /* allowEmpty */)
         , Status_(std::move(status))
         , Ready_(operation.ready())
-        , StartTime_(ProtoTimestampToInstant(operation.start_time()))
+        , CreateTime_(ProtoTimestampToInstant(operation.create_time()))
         , EndTime_(ProtoTimestampToInstant(operation.end_time()))
         , Operation_(std::move(operation))
     {
@@ -39,12 +39,16 @@ public:
         return Status_;
     }
 
-    TInstant StartTime() const {
-        return StartTime_;
+    TInstant CreateTime() const {
+        return CreateTime_;
     }
 
     TInstant EndTime() const {
         return EndTime_;
+    }
+
+    const std::string& CreatedBy() const {
+        return CreatedBy_;
     }
 
     const Ydb::Operations::Operation& GetProto() const {
@@ -55,8 +59,9 @@ private:
     const TOperationId Id_;
     const TStatus Status_;
     const bool Ready_;
-    const TInstant StartTime_;
+    const TInstant CreateTime_;
     const TInstant EndTime_;
+    const std::string CreatedBy_;
     const Ydb::Operations::Operation Operation_;
 };
 
@@ -80,12 +85,16 @@ const TStatus& TOperation::Status() const {
     return Impl_->Status();
 }
 
-TInstant TOperation::StartTime() const {
-    return Impl_->StartTime();
+TInstant TOperation::CreateTime() const {
+    return Impl_->CreateTime();
 }
 
 TInstant TOperation::EndTime() const {
     return Impl_->EndTime();
+}
+
+const std::string& TOperation::CreatedBy() const {
+    return Impl_->CreatedBy();
 }
 
 std::string TOperation::ToString() const {
