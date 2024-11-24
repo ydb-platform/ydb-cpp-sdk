bb06ab44e201bbd75711a05e3e64e51fc0ab1213
Vladislav Polyakov <polRk@ydb.tech>
2024-11-24 08:25:49 +0000

    Enhance C++ development environment by adding gdb and ccache support

--- .devcontainer/configure.sh
+++ .devcontainer/configure.sh
@@ -4,4 +4,4 @@ WORKSPACE_FOLDER=$1
 cd $1
 mkdir -p build
 git submodule update --init --recursive
-cmake --preset release-test -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+cmake --preset release-test-with-ccache-basedir -DCMAKE_EXPORT_COMPILE_COMMANDS=1
