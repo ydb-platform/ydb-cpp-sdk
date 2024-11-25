1dc3385155a00bff11b54893274b3f21acbcfbe0
Bulat Gayazov <bylatgr@gmail.com>
2024-11-25 19:22:07 +0000

    Fixed ccache usage in devcontainer

--- .devcontainer/configure.sh
+++ .devcontainer/configure.sh
@@ -4,4 +4,5 @@ WORKSPACE_FOLDER=$1
 cd $1
 mkdir -p build
 git submodule update --init --recursive
+ccache -o cache_dir=/root/.ccache
 cmake --preset release-test-with-ccache-basedir -DCMAKE_EXPORT_COMPILE_COMMANDS=1
