c0234207814b21219dba1bc01d4e939f198bbc29
Vladislav Polyakov <polRk@ydb.tech>
2025-02-16 01:47:56 +0300

    Add YDB service to devcontainer setup (#352)
    Signed-off-by: Vladislav Polyakov <polRk@ydb.tech>

--- .devcontainer/configure.sh
+++ .devcontainer/configure.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
-WORKSPACE_FOLDER=$1
 
-cd $1
 mkdir -p build
 git submodule update --init --recursive
 ccache -o cache_dir=/root/.ccache
 cmake --preset release-test-with-ccache-basedir -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+
+if which ydb > /dev/null 2>&1; then
+    ENDPOINT=$(echo ${YDB_CONNECTION_STRING_SECURE:-$YDB_CONNECTION_STRING} | awk -F/ '{print $3}')
+    DATABASE=$(echo ${YDB_CONNECTION_STRING_SECURE:-$YDB_CONNECTION_STRING} | awk -F/ '{print "/" $4}')
+    CA_FILE_OPTION=""
+
+    if [ -n "$YDB_CONNECTION_STRING_SECURE" ]; then
+        CA_FILE_OPTION="--ca-file ${YDB_SSL_ROOT_CERTIFICATES_FILE}"
+    fi
+
+    ydb config profile create local \
+        --endpoint "$ENDPOINT" \
+        --database "$DATABASE" \
+        $CA_FILE_OPTION
+
+    ydb config profile activate local
+fi
