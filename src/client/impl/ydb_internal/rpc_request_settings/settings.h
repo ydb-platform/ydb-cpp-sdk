31797acf2fc7c4acccad1d3a03db326b292402ef
Zarina Tlupova <96023685+zarinatlupova@users.noreply.github.com>
2025-04-18 20:42:27 +0300

    Update settings.h

--- src/client/impl/ydb_internal/rpc_request_settings/settings.h
+++ src/client/impl/ydb_internal/rpc_request_settings/settings.h
@@ -24,6 +24,12 @@ struct TRpcRequestSettings {
         rpcSettings.TraceId = settings.TraceId_;
         rpcSettings.RequestType = settings.RequestType_;
         rpcSettings.Header = settings.Header_;
+
+        // Add TraceParent to headers (if it is set)
+        if (!settings.TraceParent_.empty()) {
+            rpcSettings.Header.emplace_back("traceparent", settings.TraceParent_);
+        }
+        
         rpcSettings.PreferredEndpoint = preferredEndpoint;
         rpcSettings.EndpointPolicy = endpointPolicy;
         rpcSettings.UseAuth = true;
