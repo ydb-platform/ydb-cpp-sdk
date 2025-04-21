91c8cab3183aa515619275a9250d1c1ccb1280d6
Bulat Gayazov <bylatgr@gmail.com>
2025-04-21 21:06:55 +0000

    Refactor tests

--- tests/integration/basic_example/basic_example.cpp
+++ tests/integration/basic_example/basic_example.cpp
@@ -514,15 +514,3 @@ std::vector<NYdb::TResultSet> ScanQuerySelect(TTableClient client, const std::st
 
     return resultSets;
 }
-
-void DropTables(TTableClient& client, const std::string& path) {
-    ThrowOnError(client.RetryOperationSync([path](TSession session) {
-        return session.DropTable(JoinPath(path, "series")).ExtractValueSync();
-    }));
-    ThrowOnError(client.RetryOperationSync([path](TSession session) {
-        return session.DropTable(JoinPath(path, "seasons")).ExtractValueSync();
-    }));
-    ThrowOnError(client.RetryOperationSync([path](TSession session) {
-        return session.DropTable(JoinPath(path, "episodes")).ExtractValueSync();
-    }));
-}
