3d276eae593e6fc236bb12dd4ed0389f3dcc3631
Bulat <bulat@ydb.tech>
2025-04-22 14:53:41 +0000

    Moved sessions tests to C++ SDK repo (#17512)

--- cmake/external_libs.cmake
+++ cmake/external_libs.cmake
@@ -12,7 +12,6 @@ find_package(Snappy 1.1.8 REQUIRED)
 find_package(base64 REQUIRED)
 find_package(Brotli 1.1.0 REQUIRED)
 find_package(jwt-cpp REQUIRED)
-find_package(GTest REQUIRED)
 find_package(double-conversion REQUIRED)
 
 # RapidJSON
