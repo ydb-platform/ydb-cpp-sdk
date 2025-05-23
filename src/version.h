b9625de06f38b79f8f255b3b124219d6d72bd0e2
Bulat <bulat@ydb.tech>
2025-05-23 19:11:26 +0300

    Increment ydb-cpp-sdk version to v3.6.0 (#516)

--- src/version.h
+++ src/version.h
@@ -2,7 +2,7 @@
 
 namespace NYdb {
 
-inline const char* YDB_SDK_VERSION = "3.5.1";
+inline const char* YDB_SDK_VERSION = "3.6.0";
 inline const char* YDB_CERTIFICATE_FILE_KEY = "ydb_root_ca_v3.pem";
 
 } // namespace NYdb
