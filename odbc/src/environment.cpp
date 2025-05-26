51aa98b2423301afe114a6bc66919e201ca85196
Bulat Gayazov <bylatgr@gmail.com>
2025-05-26 18:34:06 +0000

    Parameter bind

--- odbc/src/environment.cpp
+++ odbc/src/environment.cpp
@@ -13,15 +13,31 @@ SQLRETURN TEnvironment::SetAttribute(SQLINTEGER attribute, SQLPOINTER value, SQL
     return SQL_SUCCESS;
 }
 
-SQLRETURN TEnvironment::GetDiagRec(SQLSMALLINT recNumber, SQLCHAR* sqlState, SQLINTEGER* nativeError, SQLCHAR* messageText, SQLSMALLINT bufferLength, SQLSMALLINT* textLength) {
-    // Заглушка
-    if (recNumber < 1 || recNumber > (SQLSMALLINT)Errors_.size()) return SQL_NO_DATA;
+SQLRETURN TEnvironment::GetDiagRec(SQLSMALLINT recNumber,
+                                   SQLCHAR* sqlState,
+                                   SQLINTEGER* nativeError,
+                                   SQLCHAR* messageText,
+                                   SQLSMALLINT bufferLength,
+                                   SQLSMALLINT* textLength) {
+
+    if (recNumber < 1 || recNumber > (SQLSMALLINT)Errors_.size()) {
+        return SQL_NO_DATA;
+    }
+
     const auto& err = Errors_[recNumber-1];
-    if (sqlState) strncpy((char*)sqlState, err.SqlState.c_str(), 6);
-    if (nativeError) *nativeError = err.NativeError;
+    if (sqlState) {
+        strncpy((char*)sqlState, err.SqlState.c_str(), 6);
+    }
+
+    if (nativeError) {
+        *nativeError = err.NativeError;
+    }
+
     if (messageText && bufferLength > 0) {
         strncpy((char*)messageText, err.Message.c_str(), bufferLength);
-        if (textLength) *textLength = (SQLSMALLINT)std::min((int)err.Message.size(), (int)bufferLength);
+        if (textLength) {
+            *textLength = (SQLSMALLINT)std::min((int)err.Message.size(), (int)bufferLength);
+        }
     }
     return SQL_SUCCESS;
 }
@@ -35,4 +51,4 @@ void TEnvironment::ClearErrors() {
 }
 
 } // namespace NOdbc
-} // namespace NYdb 
+} // namespace NYdb
