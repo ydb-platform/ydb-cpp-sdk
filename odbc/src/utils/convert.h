0e0cbcb336fe629f81cbd41a53f80e288bec54d9
Bulat Gayazov <bylatgr@gmail.com>
2025-06-02 23:27:43 +0000

    step

--- odbc/src/utils/convert.h
+++ odbc/src/utils/convert.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "bindings.h"
+
 #include <ydb-cpp-sdk/client/params/params.h>
 
 #include <sql.h>
@@ -7,26 +9,6 @@
 
 namespace NYdb {
 namespace NOdbc {
-
-struct TBoundParam {
-    SQLUSMALLINT ParamNumber;
-    SQLSMALLINT InputOutputType;
-    SQLSMALLINT ValueType;
-    SQLSMALLINT ParameterType;
-    SQLULEN ColumnSize;
-    SQLSMALLINT DecimalDigits;
-    SQLPOINTER ParameterValuePtr;
-    SQLLEN BufferLength;
-    SQLLEN* StrLenOrIndPtr;
-};
-
-struct TBoundColumn {
-    SQLUSMALLINT ColumnNumber;
-    SQLSMALLINT TargetType;
-    SQLPOINTER TargetValue;
-    SQLLEN BufferLength;
-    SQLLEN* StrLenOrInd;
-};
 
 SQLRETURN ConvertParam(const TBoundParam& param, TParamValueBuilder& builder);
 SQLRETURN ConvertColumn(TValueParser& parser, SQLSMALLINT targetType, SQLPOINTER targetValue, SQLLEN bufferLength, SQLLEN* strLenOrInd);
