ef57925f82d4000e5190e1f8f110bb3c4963ad23
Bulat <bulat@ydb.tech>
2025-10-31 13:42:02 +0000

    Propagate deadline in SDK components (#27497)

--- src/client/common_client/impl/iface.h
+++ src/client/common_client/impl/iface.h
@@ -1,14 +1,15 @@
 #pragma once
 
+#include <src/library/time/time.h>
+
 #include <functional>
-#include <util/datetime/base.h>
 
 namespace NYdb::inline V3 {
 
 class IClientImplCommon {
 public:
     virtual ~IClientImplCommon() = default;
-    virtual void ScheduleTask(const std::function<void()>& fn, TDuration timeout) = 0;
+    virtual void ScheduleTask(const std::function<void()>& fn, TDeadline::Duration timeout) = 0;
 };
 
 }
