1a9b85a3b562506f2fe7c72e8a2546c68651617a
Nikolay Perfilov <pnv1@yandex-team.ru>
2026-05-02 08:44:53 +0000

    Fix ub in ydb operation get (#38709)

--- src/client/topic/impl/write_session.cpp
+++ src/client/topic/impl/write_session.cpp
@@ -4,6 +4,8 @@
 #include <src/client/topic/common/simple_blocking_helpers.h>
 #include <src/library/decimal/yql_decimal.h>
 
+#include <ydb/library/persqueue/constants.h>
+
 #include <library/cpp/threading/future/wait/wait.h>
 #include <library/cpp/threading/future/subscription/wait_any.h>
 
@@ -15,11 +17,9 @@
 
 namespace NYdb::inline V3::NTopic {
 
-constexpr std::string_view WRITE_SESSION_ATTRIBUTE_TRACK_PRODUCER_ID_IN_TX = "track_producer_id_in_tx";
-
 TWriteSessionSettings& TWriteSessionSettings::SetTrackProducerIdInTx(bool value)
 {
-    const auto& key = WRITE_SESSION_ATTRIBUTE_TRACK_PRODUCER_ID_IN_TX;
+    const auto& key = NPersQueue::WRITE_SESSION_ATTRIBUTE_TRACK_PRODUCER_ID_IN_TX;
     return AppendSessionMeta({key.data(), key.size()},
                              value ? "true" : "false");
 }
