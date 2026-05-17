205fbc153996a0af2f87522ffc9c6378d318439f
Artem Ermoshkin <shfdis@yandex-team.ru>
2026-05-17 23:10:07 +0300

    first raw version of a debian package

--- cmake/PackSDK.cmake
+++ cmake/PackSDK.cmake
@@ -7,17 +7,26 @@ set(CPACK_PACKAGE_DESCRIPTION
     with strict consistency and ACID transactions."
 )
 
-set(CPACK_PACKAGE_NAME "ydb-cpp-sdk-dev")
+set(CPACK_PACKAGE_NAME "ydb-cpp-sdk")
 set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
 set(CPACK_GENERATOR "DEB")
 set(CPACK_PACKAGE_VERSION "${YDB_SDK_VERSION}")
 set(CPACK_PACKAGE_HOMEPAGE_URL "https://ydb.tech")
 set(CPACK_PACKAGE_CONTACT "YDB Team <whoami@where>") # TODO
 set(CPACK_RESOURCE_FILE_LICENSE "${YDB_SDK_SOURCE_DIR}/LICENSE") 
-# TODO add more parameters
 
-# TODO specify package dependencies
-# set(CPACK_DEBIAN_PACKAGE_DEPENDS "liblz4-dev libdouble-conversion-dev libssl-dev")
+set(CPACK_DEB_COMPONENT_INSTALL ON)
+set(CPACK_COMPONENTS_ALL libydb-cpp libydb-cpp-iam libydb-cpp-otel-metrics libydb-cpp-otel-tracing)
 
-# TODO use lintian in CI to check deb package
+set(CPACK_DEBIAN_LIBYDB_CPP_PACKAGE_NAME "libydb-cpp-dev")
+
+set(CPACK_DEBIAN_LIBYDB_CPP_IAM_PACKAGE_NAME "libydb-cpp-iam-dev")
+set(CPACK_DEBIAN_LIBYDB_CPP_IAM_PACKAGE_DEPENDS "libydb-cpp-dev (= ${YDB_SDK_VERSION})")
+
+set(CPACK_DEBIAN_LIBYDB_CPP_OTEL_METRICS_PACKAGE_NAME "libydb-cpp-otel-metrics-dev")
+set(CPACK_DEBIAN_LIBYDB_CPP_OTEL_METRICS_PACKAGE_DEPENDS "libydb-cpp-dev (= ${YDB_SDK_VERSION})")
+
+set(CPACK_DEBIAN_LIBYDB_CPP_OTEL_TRACING_PACKAGE_NAME "libydb-cpp-otel-tracing-dev")
+set(CPACK_DEBIAN_LIBYDB_CPP_OTEL_TRACING_PACKAGE_DEPENDS "libydb-cpp-dev (= ${YDB_SDK_VERSION})")
+
 include(CPack)
