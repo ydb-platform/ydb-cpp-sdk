143566d65f8a546579e22dd28d60253d60114eb1
Artem Ermoshkin <shfdis@yandex-team.ru>
2026-07-02 16:20:43 +0300

    update caching and ci behaviour

--- tests/slo_workloads/userver/cmake/SetupYdbCppSDK.cmake
+++ tests/slo_workloads/userver/cmake/SetupYdbCppSDK.cmake
@@ -1,8 +1,8 @@
-# Override userver's SetupYdbCppSDK.cmake: use in-tree YDB-CPP-SDK targets
-# from this repo instead of downloading ydb-cpp-sdk via CPM.
+# Override userver's SetupYdbCppSDK.cmake: use already configured
+# YDB-CPP-SDK targets instead of downloading ydb-cpp-sdk via CPM.
 
 if(NOT TARGET YDB-CPP-SDK::Table)
-    message(FATAL_ERROR "In-tree YDB-CPP-SDK must be configured before building userver-ydb")
+    message(FATAL_ERROR "YDB-CPP-SDK::Table must be configured before building userver-ydb")
 endif()
 
 set(ydb-cpp-sdk_INCLUDE_DIRS "")
