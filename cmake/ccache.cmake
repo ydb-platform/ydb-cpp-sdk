5fbd1f2280108a425f135a87b6bbd925b308489b
Artem Ermoshkin <shfdis@yandex-team.ru>
2026-08-06 12:49:37 +0300

    fix ci caches

--- cmake/ccache.cmake
+++ cmake/ccache.cmake
@@ -1,7 +1,5 @@
 find_program(CCACHE_PATH ccache)
-if (YDB_SDK_COVERAGE)
-  message(STATUS "YDB_SDK_COVERAGE is ON: ccache compiler launchers disabled")
-elseif (NOT CCACHE_PATH)
+if (NOT CCACHE_PATH)
   message(AUTHOR_WARNING
     "Ccache is not found, that will increase the re-compilation time; "
     "pass -DCCACHE_PATH=path/to/bin to specify the path to the `ccache` binary file."
